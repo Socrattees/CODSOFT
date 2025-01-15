f28921f3e4e4479646a2158730b2bede8ad305d3
Socrattees <qiniselad@gmail.com>
2025-01-15 14:27:01 +0200

    Create basic layout for Navbar

---
+++ level-3/task-1/client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <a href="/">
+          <img className="navbar-logo" src="/victory_vault_logo.png" alt="Victory Vault" />
+        </a>
+      <input className="navbar-search" type="text" placeholder="Search..." />
+      <ul className="navbar-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/login">Login</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
